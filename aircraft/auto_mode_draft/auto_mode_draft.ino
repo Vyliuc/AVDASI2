9ba9d69e732f1159c4fbf414e861b8744a33d8b8
Abdirahman Sheik Hassan <ash20701@gmail.com>
2021-10-23 01:48:09 +0100

    Verified form of stabilisation draft
    Still not to be pushed to aircraft

--- aircraft/auto_mode_draft/auto_mode_draft.ino
+++ aircraft/auto_mode_draft/auto_mode_draft.ino
@@ -1,20 +1,20 @@
-#include <tranceiver.h>
+#include <transceiver.h>
 #include <Servo.h>
 
-float V = 20
-float St = 0.141
-float at = 4.079
-float adel = 2.37
-float xg = 31.75
-float xtail = 815
-float m = 10
-float it = -2
-float const g = 9.80665
-float const rho = 1.225
-float q = 0.5*rho*(V^2)
-float def = 0
-float const lim = 3
-//float M = //REQUEST FROM CAD TEAM
+float V = 20;
+float St = 0.141;
+float at = 4.079;
+float adel = 2.37;
+float xg = 31.75;
+float xtail = 815;
+float m = 10;
+float it = -2;
+float const g = 9.80665;
+float const rho = 1.225;
+float q = 0.5*rho*(pow(V,2));
+float def = 0;
+float const lim = 3;
+//float M = ; //REQUEST FROM CAD TEAM
 
 
 //initialise servo
@@ -27,20 +27,20 @@ void setup() {
 
 void loop() {
   //receive values for pitch data
-  //ang = 
-  //angvel =
-  //angacc = 
+  //float ang = ;
+  //float angvel = ;
+  //float angacc = ;
   
   // stability reference
-  ref = (q*xtail*St*((at*(ang+it+((angvel*xtail)/V)))+(adel*def)))+(xg*m*g)- M*angacc;
+  float ref = (q*xtail*St*((at*(ang+it+((angvel*xtail)/V)))+(adel*def)))+(xg*m*g)- M*angacc;
 
   //while reference is non zero, loop over moment balance until it's zero
   if(ref != 0) {
     //run moment balance
-    def = (((M*angacc)/(q*xtail*St))-((xg*mg)/(q*xtail*St))-(at*ang)-(at*((angvel*xtail)/(V^2)))-(at*it))/adel;
+    float def = (((M*angacc)/(q*xtail*St))-((xg*m*g)/(q*xtail*St))-(at*ang)-(at*((angvel*xtail)/(pow(V,2))))-(at*it))/adel;
     
     //write deflection onto servo after some scaling and limiting
-    if (abs(def) =< lim) {
+    if (abs(def) <= lim) {
       elevator.write(def);
     }
     else if (def > lim) {
@@ -54,6 +54,5 @@ void loop() {
   //when reference reaches zero, stop deflecting angle 
   else {
     elevator.write(0);
-  }
   }
 }
